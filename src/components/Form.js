0569bfaf1ef2f26d2c79db3ce1a927149c8ac102
MMendes24 <mmbusiness1@protonmail.com>
2020-12-01 22:14:13 -0600

    Imported dependencies

---
+++ src/components/Form.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Grid, TextField, Button, Dialog, DialogContent, Typography, DialogTitle } from '@material-ui/core';
+
+const Form = () => {
+    return (
+        <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+        >
+            <Typography variant="h3" gutterBottom>Contact MK Decision</Typography>
+            <form>
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                >
+
+                </Grid>
+            </form>
+        </Grid>
+    )
+}
+
+export default Form
